e4e6e86613d0e7fcb6bd2da116d47c65b86dfa9c
Nate Wright <natewr@users.sourceforge.net>
2014-05-14 11:30:54 +0100

    * Remove grunt less/js commands

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,55 +10,6 @@ module.exports = function(grunt) {
 		// Load grunt project configuration
 		pkg: grunt.file.readJSON('package.json'),
 
-		// Configure less CSS compiler
-		less: {
-			build: {
-				options: {
-					compress: true,
-					cleancss: true,
-					ieCompat: true
-				},
-				files: {
-					'good-reviews-wp/assets/css/style.css': [
-						'good-reviews-wp/assets/src/less/style.less',
-						'good-reviews-wp/assets/src/less/style-*.less'
-					]
-				}
-			}
-		},
-
-		// Configure JSHint
-		jshint: {
-			test: {
-				src: 'good-reviews-wp/assets/src/js/*.js'
-			}
-		},
-
-		// Concatenate scripts
-		concat: {
-			build: {
-				files: {
-					'good-reviews-wp/assets/js/frontend.js': [
-						'good-reviews-wp/assets/src/js/frontend.js',
-						'good-reviews-wp/assets/src/js/frontend-*.js'
-					]
-				}
-			}
-		},
-
-		// Minimize scripts
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-			},
-			build: {
-				files: {
-					'good-reviews-wp/assets/js/frontend.js' : 'good-reviews-wp/assets/src/js/frontend.js',
-					'good-reviews-wp/assets/js/admin.js' : 'good-reviews-wp/assets/src/js/admin.js'
-				}
-			}
-		},
-
 		sync: {
 			main: {
 				files: [
@@ -73,16 +24,8 @@ module.exports = function(grunt) {
 
 		// Watch for changes on some files and auto-compile them
 		watch: {
-			less: {
-				files: ['good-reviews-wp/assets/src/less/*.less'],
-				tasks: ['less', 'sync']
-			},
-			js: {
-				files: ['good-reviews-wp/assets/src/js/*.js'],
-				tasks: ['jshint', 'concat', 'uglify', 'sync']
-			},
 			sync: {
-				files: ['!good-reviews-wp/**/*.less', '!good-reviews-wp/**/*.css', '!good-reviews-wp/**/*.js', 'good-reviews-wp/**'],
+				files: ['good-reviews-wp/**'],
 				tasks: ['sync']
 			}
 		}
